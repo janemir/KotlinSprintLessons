49b2e1017e03d2888352dc33bc1686de27313d60
janemir <janemiro2005@gmail.com>
2026-03-10 20:29:17 +0300

    переменные

---
+++ src/Main.kt
@@ -0,0 +1,34 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var numberOfOrders: Int  = 42
+    val welcomeMessage: String = "Welcome to our app!"
+
+//    println(numberOfOrders)
+    println(welcomeMessage)
+
+    numberOfOrders = 52
+    println(numberOfOrders)
+
+    // const val - константа
+
+    // целочисленные
+    val intNum1: Int = -2147483648 // 32 bit
+    val intNum2: Int = 2147483647 // 32 bit
+    val longNum: Long // 64 bit
+    val shortNum: Short // 16 bit
+    val byteNum: Byte // 8 bit
+    val unsigned: UInt = 2147483647u // 32 bit
+
+    // вещественные
+    val doubleNum: Double = 234.42 // 64 bit
+    val floatNum: Float = 876.3f // 32 bit
+
+    // строковые
+    val stringValue: String = "this is string"
+    val charValue: Char = '1'
+
+    // логические
+    val trueValue: Boolean = true
+    val falseValue: Boolean = false
+}
