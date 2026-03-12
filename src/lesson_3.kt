2dfd6796004170fea9b00fea0a91cc6275bcc28e
janemir <janemiro2005@gmail.com>
2026-03-12 22:13:51 +0300

    строки

---
+++ src/lesson_3.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+    val greeting = "Hello"
+    val space = ' '
+    val userName = "Star Lord"
+
+    // конкатенация строк
+    println(greeting +  ", " + userName + "!" + space + "What is your main question?")
+
+    //интерполяция строк
+    println("$greeting, $userName! What is your main question?")
+    println("To which you can respond ${40 + 2}")
+
+    val multistring = """
+                    |aaaaaaa
+               |bbbbbbb
+                    |cccc
+    """.trimMargin()
+
+    println(multistring)
+
+    val phrase = "And he said: \"It is magnificent!\""
+    println(phrase)
+
+}
