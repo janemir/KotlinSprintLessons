657ff8438a9d39b02632c4828ec835e9fc3c99ef
janemir <janemiro2005@gmail.com>
2026-07-10 23:57:53 +0300

    while

---
+++ src/lesson_6.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    var counter = 5
+
+    while (counter > 0) {
+        println("Реклама закончится через ${counter--}")
+        Thread.sleep(1000)
+    }
+
+//    do {
+//        println("Реклама закончится через ${counter--}")
+//        Thread.sleep(1000)
+//    } while (false)
+}
