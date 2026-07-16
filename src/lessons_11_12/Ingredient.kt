fced994d7fd55cf1f0408f7f2ee070f9af4ae02f
janemir <janemiro2005@gmail.com>
2026-07-17 00:06:31 +0300

    вторичный конструктор

--- src/lessons_11_12/Ingredient.kt
+++ src/lessons_11_12/Ingredient.kt
@@ -1,9 +1,7 @@
 package lessons_11_12
 
-class Ingredient(_name: String, _weight: Int, _count: Int) {
+class Ingredient(val _name: String, val _weight: Int, val _count: Int) {
 
-    var name = _name
-    var weight = _weight
-    var count = _count
+
 
 }
