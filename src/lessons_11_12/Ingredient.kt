5dd084a004fdfc4e0ef8796f51847762a0c37f4a
janemir <janemiro2005@gmail.com>
2026-07-17 23:47:19 +0300

    инициализатор

--- src/lessons_11_12/Ingredient.kt
+++ src/lessons_11_12/Ingredient.kt
@@ -1,7 +1,6 @@
 package lessons_11_12
 
 class Ingredient(val _name: String, val _weight: Int, val _count: Int) {
-<<<<<<< HEAD
 
     var isNeedToPrepare = false
 
@@ -15,9 +14,9 @@ class Ingredient(val _name: String, val _weight: Int, val _count: Int) {
         isNeedToPrepare = _isNeedToPrepare
 
     }
-=======
 
-
->>>>>>> fced994d7fd55cf1f0408f7f2ee070f9af4ae02f
+    init {
+        println("Ингредиент $_name создан")
+    }
 
 }
