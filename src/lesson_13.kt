ba1dda5a8e7822e2c1d033947cfb2e7247e1efbd
janemir <janemiro2005@gmail.com>
2026-07-19 22:51:21 +0300

    null safety

---
+++ src/lesson_13.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    val nullableString: String? = null
+//    val nonNullableString: String = "some string"
+
+//    println(nullableString?.length)
+//    println(nonNullableString.length)
+
+//    if(nullableString != null) println(nullableString.length)
+//    else println("Переменная хранит null")
+
+//    val nullableLine: String? = readLine()
+
+//    val stringLength: Int = nullableString?.length ?: 999
+//    println(stringLength)
+
+    val stringLength: Int = nullableString!!.length
+}
